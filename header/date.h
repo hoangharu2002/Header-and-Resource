0625a49abe9042dcfb6e048e79a2223b707391a5
Just4Func <nnhoang2002@gmail.com>
2023-07-31 20:58:32 +0700

    uodate header and source for Date struct

---
+++ header/date.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _day_H_
+#define _day_H_
+
+#include <iostream>
+using namespace std;
+
+typedef struct Date {
+	unsigned int day, month, year;
+};
+
+bool checkLeapYear(Date today)
+
+#endif
